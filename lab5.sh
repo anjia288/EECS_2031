937d955fda45b6f5945c2dc9ac81ca8865da6c18
Jia An <jiaan@red.eecs.yorku.ca>
2020-07-07 18:54:45 -0400

    My version

---
+++ lab5.sh
@@ -0,0 +1,24 @@
+#!bin/sh
+#jiaan
+#215257801
+
+#To sum 1~100
+
+
+count=0
+
+for ((i=1;i<=100;i++))
+do
+   count=$[$count+$i]
+
+done
+
+echo "sum=$count"
+
+echo $lab5
+
+echo "Some text for the second commit"
+
+echo "For the third commit"
+
+echo "Changed Oline"
